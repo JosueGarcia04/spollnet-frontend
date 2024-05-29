25da720b62e6c7c18f519b4aa2b26ee879f8abc3
JosueGarcia04 <josueadrian042006@gmail.com>
2024-05-29 11:41:27 -0600

    ya funcionan sususu

--- backend/controllers/forms/login.mjs
+++ backend/controllers/forms/login.mjs
@@ -1,29 +1,22 @@
-import mongoose from 'mongoose'
-import db from '../../index.mjs'
-import {Student} from '../../models/student.mjs'
-import bcrypt from 'bcrypt'
+import { Student } from "../../models/student.mjs";
+import bcrypt from "bcrypt";
 
-export const credentials = async (req, res)=>{
-    const {email, password} = req.body;
-    try{
-        await mongoose.connect(db);
-
-        const student = await Student.findOne({ email });
-        if (!student) {
-            return res.status(400).json({ msg: 'Correo no encontrado' });
-        }
-
-        const findPassword = await bcrypt.compare (password, student.password)
-        if(!findPassword){
-            return res.status(400).json({ msg: 'contraseña incorrecta' });
-        }
-
-
-        return res.status(200).json({msg: 'logueado exitosamente'});
-    }catch (err){
-        console.error('error a encontrar el usuario:', err);
-        res.status(500).json({ msg: 'Error interno del servidor' });
-    }finally{
-        mongoose.connection.close();
+export const credentials = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const student = await Student.findOne({ email });
+    if (!student) {
+      return res.status(400).json({ msg: "Correo no encontrado" });
     }
+
+    const findPassword = await bcrypt.compare(password, student.password);
+    if (!findPassword) {
+      return res.status(400).json({ msg: "contraseña incorrecta" });
+    }
+
+    return res.status(200).json({ msg: "logueado exitosamente" });
+  } catch (err) {
+    console.error("error a encontrar el usuario:", err);
+    res.status(500).json({ msg: "Error interno del servidor" });
+  } 
 };
