3d67e6d60ce73549c9fad7cf02c57610e51ddb18
Leonelcj255 <105886610+Leonelcj255@users.noreply.github.com>
2024-05-30 07:40:34 -0600

    index de spollnet
    trabje duro para esto no como amilcar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+<<<<<<< Updated upstream
     extend: {},
+=======
+    extend: {
+      colors: {
+        customBlack: '#090909', 
+      },
+      fontFamily: {
+        sans: ['YourSansFont', 'sans-serif'],
+        serif: ['YourSerifFont', 'serif'],
+        mono: ['YourMonoFont', 'monospace'],
+        extrabold: ['YourExtraboldFont', 'sans-serif'],
+      },
+    },
+>>>>>>> Stashed changes
   },
   plugins: [],
 }
+
 
