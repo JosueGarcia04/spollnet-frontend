3933ea0902360afe3cf04079ab62139c21579c71
JosueGarcia04 <josueadrian042006@gmail.com>
2024-06-17 11:38:34 -0600

    finish news (backend later)

---
+++ frontend/src/components/news/contentNews.jsx
@@ -0,0 +1,49 @@
+export default function ContentNews(){
+    return(
+        <div className='bg-black flex flex-wrap justify-center mt-10'>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
+        <img class="w-full" src="https://www.adslzone.net/app/uploads/2019/10/evoting_1200-715x402.jpg" alt="Sunset in the mountains"/>
+      <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 text-[#E31FAE]">Votaciones</div>
+        <p class="text-white text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-[#380B99] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#votes</span>
+        <span class="inline-block bg-[#380B99]  rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#students</span>
+        <span class="inline-block bg-[#380B99] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#college</span>
+      </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
+      <img class="w-full" src="https://www.adslzone.net/app/uploads/2019/10/evoting_1200-715x402.jpg" alt="Sunset in the mountains"/>
+    <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2 text-[#E31FAE]">Votaciones</div>
+      <p class="text-white text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-[#380B99] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#votes</span>
+      <span class="inline-block bg-[#380B99]  rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#students</span>
+      <span class="inline-block bg-[#380B99] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#college</span>
+    </div>
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
+      <img class="w-full" src="https://www.adslzone.net/app/uploads/2019/10/evoting_1200-715x402.jpg" alt="Sunset in the mountains"/>
+    <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2 text-[#E31FAE]">Votaciones</div>
+      <p class="text-white text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-[#380B99] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#votes</span>
+      <span class="inline-block bg-[#380B99]  rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#students</span>
+      <span class="inline-block bg-[#380B99] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">#college</span>
+    </div>
+    </div>
+  </div>
+    );
+}
+
--- frontend/src/pages/by us/general/news.jsx
+++ frontend/src/pages/by us/general/news.jsx
@@ -2,27 +2,15 @@ import React from 'react'
 import Footer from '../../../components/general/footer'
 import Navbar  from '../../../components/general/navBar'
 import NavDown from '../../../components/general/navDown'
+import ContentNews from '../../../components/news/contentNews'
 export const News = () =>{
   return(
-    <div className='bg-black'>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div>
       <Navbar/>
-    <img class="w-full" src="https://www.adslzone.net/app/uploads/2019/10/evoting_1200-715x402.jpg" alt="Sunset in the mountains"/>
-    <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2 text-[#]">Votaciones</div>
-      <p class="text-gray-700 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-      </p>
-    </div>
-    <div class="px-6 pt-4 pb-2">
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-    </div>
+      <ContentNews/>
+      <NavDown/>
+      <Footer/>
   </div>
-  <NavDown/>
-  <Footer/>
-    </div>
   );
 }
 
