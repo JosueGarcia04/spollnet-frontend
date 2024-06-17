7e5fc279eef4d38f22fa199b547d5c427a5b0d54
JosueGarcia04 <josueadrian042006@gmail.com>
2024-06-17 10:02:43 -0600

    position in footer

--- frontend/src/components/general/footer.jsx
+++ frontend/src/components/general/footer.jsx
@@ -5,6 +5,7 @@ import LogoNode from '../svg/logoNode'
 import LogoExpress from '../svg/logoExpress'
 import LogoReact from '../svg/logoReact'
 import LogoMongo from '../svg/logoMongo'
+import LogoJavascript from '../svg/logoJs'
 
 const Footer = () => {
   return(
@@ -19,23 +20,23 @@ const Footer = () => {
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
               <div>
-                  <h2 class="mb-6 text-xl font-bold text-[#E31FAE]">Acerca de</h2>
-                  <ul class="text-white font-medium">
-                      <li class="mb-4">
+                  <h2 class="mb-6 text-xl font-bold text-[#E31FAE] text-center">Acerca de</h2>
+                  <ul class="text-white font-medium text-center ">
+                      <li class="mb-4 hover:text-[#E31FAE]">
                           <Link to={"/about us"}>Nosotros</Link>
                       </li>
-                      <li>
+                      <li className='hover:text-[#E31FAE]'>
                           <Link to={"/faq"}>Preguntas frecuentes</Link>
                       </li>
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-xl font-bold text-[#E31FAE]">Legal</h2>
-                  <ul class="text-white font-medium">
-                      <li class="mb-4">
+                  <h2 class="mb-6 text-xl font-bold text-[#E31FAE] text-center">Legal</h2>
+                  <ul class="text-white font-medium text-center">
+                      <li class="mb-4 hover:text-[#E31FAE]">
                           <Link to={"#"}>Politica de Privacidad</Link>
                       </li>
-                      <li>
+                      <li class='hover:text-[#E31FAE]'>
                           <Link to={"#"}>Terminos y condiciones</Link>
                       </li>
                   </ul>
@@ -43,29 +44,34 @@ const Footer = () => {
           </div>
       </div>
       <hr className="my-8 border-[#380B99] border-solid border-t-4" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-          <span className=" text-[#E32FAE] text-center">
-            © 2024 Spollnet. Todos los derechos reservados.
+      <div class="sm:flex sm:items-center sm:justify-between text-center">
+          <span className="text-[#E32FAE] text-xl font-bold">
+            Todos los derechos reservados.
           </span>
-          <div className="flex mt-5 sm:justify-center">
-            <Link to={"https://github.com/JosueGarcia04/spollnet-crea-J-2024"} className="text-white ms-5">
+          <div className="flex justify-center mt-5 sm:mt-0">
+            <Link to={"https://github.com/JosueGarcia04/spollnet-crea-J-2024"} 
+            className="text-white mx-5">
                 <LogoGithub></LogoGithub>
             </Link>
             <Link to={"https://nodejs.org/en"}
-             className="text-white ms-5">
+             className="text-white mx-5">
                 <LogoNode></LogoNode>
             </Link>
             <Link to={"https://expressjs.com/"}
-             className="text-white ms-5">
+             className="text-white mx-5">
                 <LogoExpress></LogoExpress>
             </Link>
             <Link to={"https://react.dev/"}
-             className="text-white ms-5">
+             className="text-white mx-5">
                 <LogoReact></LogoReact>
             </Link>
             <Link to={"https://www.mongodb.com/"}
-             className="text-white ms-5">
+             className="text-white mx-5">
                 <LogoMongo></LogoMongo>
+            </Link>
+            <Link to={"https://www.javascript.com/"}
+             className="text-white mx-5">
+                <LogoJavascript></LogoJavascript>
             </Link>
          </div>
       </div>
--- frontend/src/pages/by us/general/news.jsx
+++ frontend/src/pages/by us/general/news.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Footer from '../../../components/general/footer'
+import Navbar  from '../../../components/general/navBar'
+import NavDown from '../../../components/general/navDown'
 export const News = () =>{
   return(
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
+    <div className='bg-black'>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <Navbar/>
+    <img class="w-full" src="https://www.adslzone.net/app/uploads/2019/10/evoting_1200-715x402.jpg" alt="Sunset in the mountains"/>
     <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <div class="font-bold text-xl mb-2 text-[#]">Votaciones</div>
       <p class="text-gray-700 text-base">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
       </p>
@@ -15,6 +20,9 @@ export const News = () =>{
       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
     </div>
   </div>
+  <NavDown/>
+  <Footer/>
+    </div>
   );
 }
 
