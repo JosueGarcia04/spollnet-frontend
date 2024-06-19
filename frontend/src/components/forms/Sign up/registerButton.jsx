de53624b50b22505b33a0703d99555c2cf648a14
JosueGarcia04 <josueadrian042006@gmail.com>
2024-06-19 00:12:38 -0600

    button (sad face)

--- frontend/src/components/forms/Sign up/registerButton.jsx
+++ frontend/src/components/forms/Sign up/registerButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const RegisterButton = () => {
     return (
     <div className="flex justify-end mt-6">
-        <button className="bg-[#380B99] hover:bg-[#361d6c] text-white font-bold py-3 px-3.5 w-full border-b-4 border-[#6635d1] hover:border-[#5229ab] rounded">Regístrate</button>
+        <button className="bg-[#E41FAE] text-white px-6 py-3 rounded-md shadow-md hover:bg-[#d81b9a] font-bold transition-colors duration-300 w-full">Regístrate</button>
     </div>
     );
 }
