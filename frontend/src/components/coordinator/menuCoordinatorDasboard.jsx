35e27b8519f6fb2ace70232ce727ebd2be931618
JosueGarcia04 <josueadrian042006@gmail.com>
2024-06-18 18:00:31 -0600

    update in p from menu

--- frontend/src/components/coordinator/menuCoordinatorDasboard.jsx
+++ frontend/src/components/coordinator/menuCoordinatorDasboard.jsx
@@ -3,118 +3,63 @@ import { faHouse, faUsers, faNewspaper, faChartSimple, faFile, faCalendarCheck, 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function MenuCoordinatorDashboard(){
     return(
-        <div>
-            <div id="menu" class="flex flex-col space-y-2 w-full ">
-            <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faHouse}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Panel principal</p>
-                       
-                        </div>
-                        
+        <div className='h-screen flex flex-col'>
+            <div id="menu" class="flex flex-col space-y-2 w-full">
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faHouse}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Inicio</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faInbox}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Notificaciones</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faInbox}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Notis</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faUsers}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Centro de usuarios</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faUsers}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Usuarios</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faCalendarCheck}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Periodo de votacion</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faCalendarCheck}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Periodos</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faFaceLaughBeam}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Supervisión del proceso</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faFaceLaughBeam}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Control</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faChartSimple}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Resultados</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faChartSimple}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Stats</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faFile}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Reportes</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faFile}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Reportes</p>
                     </div>
                 </a>
-                <a href="#" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faNewspaper}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Noticias</p>
-                       
-                        </div>
-                        
+                <a href="#" className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
+                    <div className="flex items-center space-x-2">
+                        <FontAwesomeIcon icon={faNewspaper}/>                           
+                        <p className="font-bold text-base lg:text-lg text-slate-200">Noticias</p>
                     </div>
                 </a>
-                <a href="#" class="bg-red-700 rounded-lg  group w-full  py-3 px-2 ">
-                    <div class="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-                        <div>
-                             <FontAwesomeIcon icon={faDoorOpen}/>                           
-                        </div>
-                        <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4">Salir</p>
-                       
-                        </div>
-                        
-                    </div>
+                <a href="#" className="bg-red-700 rounded-lg group w-full py-3 px-2">
+                <div className="flex items-center space-x-2">
+                    <FontAwesomeIcon icon={faDoorOpen}/>                           
+                    <p className="font-bold text-base lg:text-lg text-slate-200">Salir</p>
+                </div>
                 </a>
             </div>
         </div>
-        
     );
 }
