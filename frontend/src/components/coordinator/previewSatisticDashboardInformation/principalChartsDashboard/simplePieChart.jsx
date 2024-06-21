d7c9144198df5ab340794b4d7d628c814537244e
JosueGarcia04 <josueadrian042006@gmail.com>
2024-06-20 23:33:20 -0600

    bug chart fixed

--- frontend/src/components/coordinator/previewSatisticDashboardInformation/principalChartsDashboard/simplePieChart.jsx
+++ frontend/src/components/coordinator/previewSatisticDashboardInformation/principalChartsDashboard/simplePieChart.jsx
@@ -1,32 +1,35 @@
+import React from "react";
 import { Chart } from "react-google-charts";
 
-export const SimplePieChart = () =>{
-    const data = [
-        ["Manuel", 11],
-        ["Amilcarneta", 2],
-        ["Carlos y leo", 2],
-        ["picheeell", 2],
-        ["paletaa", 7],
-      ];
-    const options = {
-        backgroundColor: "#000",
-        titleTextStyle: {
-          color: 'white', 
-        },
-        legend: {
-          textStyle: {
-            color: '#fff', 
-          },
-        },
-      }
-    return(
-        <Chart
-            chartType="PieChart"
-            data={data}
-            options={options}
-            width={"100%"}
-            height={"400px"}
-        />
+export const SimplePieChart = () => {
+  const data = [
+    ["Person", "Votes"], // Encabezados de las columnas
+    ["Manuel", 11],
+    ["Amilcarneta", 2],
+    ["Carlos y leo", 2],
+    ["Picheeell", 2],
+    ["Paletaa", 7],
+  ];
 
-    );
-}
+  const options = {
+    backgroundColor: "white",
+    titleTextStyle: {
+      color: 'white', 
+    },
+    legend: {
+      textStyle: {
+        color: '#fff', 
+      },
+    },
+  };
+
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
+};
