78748fdf838dae16cf1411a89e6885f4c3c7039f
JosueGarcia04 <josueadrian042006@gmail.com>
2024-06-29 00:27:44 -0600

    many validations

--- frontend/src/pages/forms/Sign-in.jsx
+++ frontend/src/pages/forms/Sign-in.jsx
@@ -33,15 +33,6 @@ const SignIn = () => {
         if (year < 2010 || year > 2024) return false; 
         return true;
     };
-    const isValidEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    };
-
-    const isValidPassword = (password) => {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        return passwordRegex.test(password);
-    };
     const checkCarnetUnique = async (carnet) => {
         try {
             const response = await axios.post('http://localhost:5000/check-carnet', { carnet });
@@ -51,10 +42,31 @@ const SignIn = () => {
             return false;
         }
     };
+
     const isValidName = (name) => {
        
         const nameRegex = /^[a-zA-Z\s]+$/;
         return nameRegex.test(name);
+    };
+
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const isValidPassword = (password) => {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return passwordRegex.test(password);
+    };
+
+    const checkPasswordUnique = async (password) => {
+        try {
+            const response = await axios.post('http://localhost:5000/check-password', { password });
+            return response.data.isUnique;
+        } catch (error) {
+            console.error('Error checking password uniqueness:', error);
+            return false;
+        }
     };
 
     const validations = async () => {
@@ -62,82 +74,58 @@ const SignIn = () => {
         if (!name) {
             errors.name = 'Completa el campo de nombre';
         } else if (!isValidName(name)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Nombre incorrecto',
-                text: 'El nombre solo puede contener letras y espacios.',
-            });
-            errors.name = 'Nombre incorrecto';
+            errors.name = 'El nombre solo puede contener letras y espacios.';
         }
         if (!mail) {
             errors.mail = 'Completa el campo de correo electrónico';
         } else if (!isValidEmail(mail)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Correo electrónico incorrecto',
-                text: 'El correo debe tener un formato válido (ejemplo@correo.com).',
-            });
-            errors.mail = 'Correo electrónico incorrecto';
+            errors.mail = 'El correo debe tener un formato válido.';
         }
+        
         if (!contra) {
             errors.contra = 'Completa el campo de contraseña';
         } else if (!isValidPassword(contra)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Contraseña incorrecta',
-                text: 'La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula, una minúscula, un número y un carácter especial.',
-            });
-            errors.contra = 'Contraseña incorrecta';
+            errors.contra = 'La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula, una minúscula, un número y un carácter especial.';
         } else {
             const isUnique = await checkPasswordUnique(contra);
             if (!isUnique) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Contraseña duplicada',
-                    text: 'La contraseña ingresada ya está registrada. Por favor ingresa una contraseña única.',
-                });
-                errors.contra = 'Contraseña duplicada';
+                errors.contra = 'La contraseña ingresada ya está registrada. Por favor ingresa una contraseña única.';
             }
         }
+
+
         if (!level) {
-            errors.level = 'Completa el campo de nivel educativo';
+            errors.level = 'Completa nivel educativo';
         } else if (!validEducationLevels.includes(level)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Nivel educativo incorrecto',
-                text: 'El nivel educativo ingresado no es válido. Los valores permitidos son: Primaria, Tercer ciclo, Bachillerato.'
-            });
-            errors.level = 'Nivel educativo incorrecto';
+            errors.level = 'El nivel educativo ingresado no es válido. Los valores permitidos son: Primaria, Tercer ciclo, Bachillerato.';
         }
+
         if (level === 'Bachillerato' && !validSpecialties.includes(specialty)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Especialidad incorrecta',
-                text: 'La especialidad ingresada no es válida. Los valores permitidos son: Mantenimiento automotriz, Desarrollo de software, Atencion primaria en salud, Electromecanica, Diseño Grafico y Electronica.'
-            });
-            errors.specialty = 'Especialidad incorrecta';
+            errors.specialty = 'La especialidad ingresada no es válida. Los valores permitidos son: Mantenimiento automotriz, Desarrollo de software, Atencion primaria en salud, Electromecanica, Diseño Grafico y Electronica.';
         }
+
         if (!identifier) {
             errors.identifier = 'Completa el campo de carnet';
         } else if (!isValidCarnet(identifier)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Carnet incorrecto',
-                text: 'El carnet ingresado no es válido. Debe tener exactamente 8 dígitos y los primeros 4 dígitos deben estar en el rango 2010-2024.'
-            });
-            errors.identifier = 'Carnet incorrecto';
+            errors.identifier = 'El carnet ingresado no es válido. Debe tener exactamente 8 dígitos y los primeros 4 dígitos deben estar en el rango 2010-2024.';
         } else {
             const isUnique = await checkCarnetUnique(identifier);
             if (!isUnique) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Carnet duplicado',
-                    text: 'El carnet ingresado ya está registrado. Por favor ingresa un carnet único.'
-                });
-                errors.identifier = 'Carnet duplicado';
+                errors.identifier = 'El carnet ingresado ya está registrado. Por favor ingresa un carnet único.';
             }
         }
+
         return errors;
+    };
+
+
+    const handleInputFocus = (field) => {
+        setErrors({ ...errors, [field]: '' });
+    };
+
+    const handleInputChange = (setter, field, value) => {
+        setter(value);
+        handleInputFocus(field);
     };
 
     const handleSubmit = async (e) => {
@@ -199,10 +187,9 @@ const SignIn = () => {
                                     id='name'
                                     name='name'
                                     type='text'
-                                    value={name} onChange={(e) => {
-                                        setNombre(e.target.value);
-                                        setErrors({ ...errors, name: '' });
-                                    }}
+                                    value={name}
+                                    onChange={(e) => handleInputChange(setNombre, 'name', e.target.value)}
+                                    className={`border-2 p-2 rounded ${errors.name ? 'border-red-500' : 'border-green-500'}`}
                                 />
                                 {errors.name && <p className="text-red-500">{errors.name}</p>}
                             </div>
@@ -212,66 +199,57 @@ const SignIn = () => {
                                     id='emailAdress'
                                     name='email'
                                     type='email'
-                                    value={mail} onChange={(e) => {
-                                        setEmail(e.target.value);
-                                        setErrors({ ...errors, mail: '' });
-                                    }}
+                                    value={mail}
+                                    onChange={(e) => handleInputChange(setEmail, 'mail', e.target.value)}
+                                    className={`border-2 p-2 rounded ${errors.mail ? 'border-red-500' : 'border-green-500'}`}
                                 />
                                 {errors.mail && <p className="text-red-500">{errors.mail}</p>}
                             </div>
                             <div className="text-center font-bold">
                                 <Label htmlFor="level">Nivel educativo </Label>
                                 <Input
-                                    id='level'
-                                    name='level'
+                                    id='educationLevel'
+                                    name='educationLevel'
                                     type='text'
-                                    value={level} onChange={(e) => {
-                                        setEducationlevel(e.target.value);
-                                        setErrors({ ...errors, level: '' });
-                                        if (e.target.value === 'Primaria' || e.target.value === 'Tercer ciclo') {
-                                            setEspecialty('');
-                                        }
-                                    }}
+                                    value={level}
+                                    onChange={(e) => handleInputChange(setEducationlevel, 'level', e.target.value)}
+                                    className={`border-2 p-2 rounded ${errors.level ? 'border-red-500' : 'border-green-500'}`}
                                 />
-                                {errors.level && <p className="text-red-500">{errors.level}</p>}
                             </div>
                             <div className="text-center font-bold">
                                 <Label htmlFor="speciality">Especialidad</Label>
                                 <Input
-                                    id='speciality'
-                                    name='speciality'
+                                    id='specialty'
+                                    name='specialty'
                                     type='text'
+                                    value={specialty}
+                                    onChange={(e) => handleInputChange(setEspecialty, 'specialty', e.target.value)}
                                     disabled={level === 'Primaria' || level === 'Tercer ciclo'}
-                                    value={specialty} onChange={(e) => {
-                                        setEspecialty(e.target.value);
-                                        setErrors({ ...errors, specialty: '' });
-                                    }}
+                                    className={`border-2 p-2 rounded ${errors.specialty ? 'border-red-500' : 'border-green-500'}`}
                                 />
                                 {errors.specialty && <p className="text-red-500">{errors.specialty}</p>}
                             </div>
                             <div className="text-center font-bold">
                                 <Label htmlFor="identify">Carnet </Label>
                                 <Input
-                                    id='identify'
-                                    name='identify'
+                                    id='identifier'
+                                    name='identifier'
                                     type='text'
-                                    value={identifier} onChange={(e) => {
-                                        setIdentifier(e.target.value);
-                                        setErrors({ ...errors, identifier: '' });
-                                    }}
+                                    value={identifier}
+                                    onChange={(e) => handleInputChange(setIdentifier, 'identifier', e.target.value)}
+                                    className={`border-2 p-2 rounded ${errors.identifier ? 'border-red-500' : 'border-green-500'}`}
                                 />
                                 {errors.identifier && <p className="text-red-500">{errors.identifier}</p>}
                             </div>
                             <div className='text-center font-bold'>
                                 <Label htmlFor="password">Contraseña </Label>
                                 <Input
-                                    id='password'
-                                    name='password'
+                                    id='contra'
+                                    name='contra'
                                     type='password'
-                                    value={contra} onChange={(e) => {
-                                        setPassword(e.target.value);
-                                        setErrors({ ...errors, contra: '' });
-                                    }}
+                                    value={contra}
+                                    onChange={(e) => handleInputChange(setPassword, 'contra', e.target.value)}
+                                    className={`border-2 p-2 rounded ${errors.contra ? 'border-red-500' : 'border-green-500'}`}
                                 />
                                 {errors.contra && <p className="text-red-500">{errors.contra}</p>}
                             </div>
