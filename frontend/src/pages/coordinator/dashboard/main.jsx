33832a89c82e22f9008f53a674b17dcef8a7af1a
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-13 01:00:33 -0600

    improving views

--- frontend/src/pages/coordinator/dashboard/main.jsx
+++ frontend/src/pages/coordinator/dashboard/main.jsx
@@ -24,11 +24,11 @@ export const Coordinator =() =>{
         <NavbarMobile/>
          <div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-7">
             <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
-                <div id="menu" class="bg-white/10 col-span-4 rounded-lg p-2 ">
+                <div id="menu" class="bg-white/10 col-span-4 rounded-lg p-2 hidden md:block ">
                 <ProfileCoordinatorDashboard onOpenModal={handleOpenModal} />
                     <MenuCoordinatorDashboard/>
                 </div>
-                <div id="content" class="bg-white/10 col-span-8 rounded-lg p-4">
+                <div id="content" className="bg-white/10 col-span-12 md:col-span-8 rounded-lg p-4">
                     <DataCoordinatorDashboard/>     
                     <PrincipalStatisticInformationComponent/>     
                 </div>
