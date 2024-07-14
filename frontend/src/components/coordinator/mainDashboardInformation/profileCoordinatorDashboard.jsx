080653cebc35cda9720bb22681d2581dff02bc62
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-13 22:06:48 -0600

    edit modal profile coordinator

--- frontend/src/components/coordinator/mainDashboardInformation/profileCoordinatorDashboard.jsx
+++ frontend/src/components/coordinator/mainDashboardInformation/profileCoordinatorDashboard.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const ProfileCoordinatorDashboard = ({ onOpenModal }) => {
   return (
+    <>
     <div onClick={onOpenModal} className="cursor-pointer">  
-      <h1 className="font-bold text-lg lg:text-3xl text-[#E31FAE]">Admin, Coordinador</h1>
+      <h1 className="font-bold text-lg lg:text-3xl text-[#E31FAE]">Dashboard Coordinador</h1>
       <br />
       <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row mb-5 items-center md:space-x-2 hover:bg-gray-700 group transition duration-150 ease-linear rounded-lg group w-full py-3 px-2">
         <div>
@@ -14,7 +15,7 @@ const ProfileCoordinatorDashboard = ({ onOpenModal }) => {
           />
         </div>
         <div>
-          <p className="font-medium leading-4 text-center">Bienvenido, Joshue</p>
+          <p className="font-medium leading-4 text-center">Hola, Coordinador</p>
           <div className="flex justify-center sm:justify-start items-center">
             <span className="inline-block mt-2 bg-green-500 rounded-full px-2 text-sm font-semibold text-white mr2">
               en línea
@@ -23,6 +24,7 @@ const ProfileCoordinatorDashboard = ({ onOpenModal }) => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
--- frontend/src/components/coordinator/mainDashboardInformation/viewProfileCoordinator/profileModalCoordinator.jsx
+++ frontend/src/components/coordinator/mainDashboardInformation/viewProfileCoordinator/profileModalCoordinator.jsx
@@ -11,7 +11,7 @@ const ProfileModal = ({ isOpen, onClose }) => {
       <div className="bg-black rounded-lg shadow-lg w-full max-w-md mx-auto p-6 relative text-white border-4 border-[#E31FAE]">
         <FontAwesomeIcon onClick={onClose} icon={faX} className="text-red-500 text-2xl absolute top-4 right-4 cursor-pointer"
         />
-        <h1 className="font-bold text-center text-lg lg:text-3xl text-[#E31FAE] mb-4">Admin, Coordinador</h1>
+        <h1 className="font-bold text-center text-lg lg:text-3xl text-[#E31FAE] mb-4">Coordinador</h1>
         <h1 className=" font-bold text-center text-lg mb-4">Colegio Don Bosco</h1>
         <div className="flex flex-col items-center mb-5">
           <img
@@ -19,7 +19,7 @@ const ProfileModal = ({ isOpen, onClose }) => {
             src="https://i.pinimg.com/originals/ad/73/1c/ad731cd0da0641bb16090f25778ef0fd.jpg"
             alt=""
           />
-          <p className="font-medium leading-4 text-center mt-4">Bienvenido, Joshue</p>
+          <p className="font-medium leading-4 text-center mt-4">coordinador123@gmail.com</p>
           <span className="inline-block mt-4 bg-green-500 rounded-full px-2 text-sm font-semibold text-white">
             en línea
           </span>
