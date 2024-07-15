b54fe7f7de3b53146df9a9e7be545f34a990b846
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-15 11:01:23 -0600

    configure the grid in dashboard

--- frontend/src/pages/coordinator/dashboard/main.jsx
+++ frontend/src/pages/coordinator/dashboard/main.jsx
@@ -9,6 +9,7 @@ import DataCoordinatorDashboard from '../../../components/coordinator/mainDashbo
 import ProfileModal from '../../../components/coordinator/mainDashboardInformation/viewProfileCoordinator/profileModalCoordinator';
 //principal statistic
 import PrincipalStatisticInformationComponent from '../../../components/coordinator/previewSatisticDashboardInformation/principalStatisticInformation'
+
 export const Coordinator =() =>{
     const [isModalOpen, setModalOpen] = useState(false);
 
@@ -19,16 +20,17 @@ export const Coordinator =() =>{
     const handleCloseModal = () => {
       setModalOpen(false);
     };
+
     return(
         <>
         <NavbarMobile/>
-         <div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-7">
-            <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
-                <div id="menu" class="bg-white/10 col-span-4 rounded-lg p-2 hidden md:block ">
-                <ProfileCoordinatorDashboard onOpenModal={handleOpenModal} />
+        <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-7">
+            <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
+                <div id="menu" className="bg-white/10 col-span-12 md:col-span-4 lg:col-span-3 rounded-lg p-2 hidden md:block">
+                    <ProfileCoordinatorDashboard onOpenModal={handleOpenModal} />
                     <MenuCoordinatorDashboard/>
                 </div>
-                <div id="content" className="bg-white/10 col-span-12 md:col-span-8 rounded-lg p-4">
+                <div id="content" className="bg-white/10 col-span-12 md:col-span-8 lg:col-span-9 rounded-lg p-4">
                     <DataCoordinatorDashboard/>     
                     <PrincipalStatisticInformationComponent/>     
                 </div>
