5f0685740e10e933dabebd3fe2b3bf2081721ee0
JosueGarcia04 <josueadrian042006@gmail.com>
2024-07-15 11:57:21 -0600

    listBannedStudents frontend

--- frontend/src/pages/coordinator/students/delete&banned/listBannedStudents.jsx
+++ frontend/src/pages/coordinator/students/delete&banned/listBannedStudents.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
+//general
+import Footer from '../../../../components/general/footer';
+import NavbarMobile from '../../../../components/coordinator/mainDashboardInformation/menuMobile/menuMobile';
+//panel content
+import MenuCoordinatorDashboard from '../../../../components/coordinator/mainDashboardInformation/menuCoordinatorDasboard';
+import UsersTableCoordinatorDashboard from '../../../../components/coordinator/studentsInformation/usersTable';
+export default function ListBannedStudents () {
+    return (
+        <>
+            <NavbarMobile />
+            <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-7">
+                <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
+                    <div id="menu" className="bg-white/10 col-span-12 md:col-span-4 lg:col-span-3 rounded-lg p-2 hidden md:block">
+                        <MenuCoordinatorDashboard />
+                    </div>
 
-
-export const ListBannedStudents =()=>{
-    return(
-        <h1>hola</h1>
+                    <div id="content" className="bg-white/10 col-span-12 md:col-span-8 lg:col-span-9 rounded-lg p-4">
+                        <UsersTableCoordinatorDashboard />
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
-
-export default ListBannedStudents;
