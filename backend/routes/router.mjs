b5262adc31e04bb4c8cca7e50e08c89e4601cf85
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-05 23:24:20 -0600

    add student running

--- backend/routes/router.mjs
+++ backend/routes/router.mjs
@@ -2,7 +2,6 @@ import express from 'express';
 import { register } from '../controllers/forms/sign_up.mjs' 
 import { credentials } from '../controllers/forms/login.mjs';
 import { checkCarnet } from '../controllers/coordinator/students/checkCarnet.mjs';
-import { addStudent } from '../controllers/coordinator/students/addStudent.mjs';
 import { deleteStudentPermanently, restoreStudent } from '../controllers/coordinator/students/restoreDelete.mjs';
 import { getAllStudents } from '../controllers/coordinator/students/getAllStudents.mjs';
 import { deleteStudent, banStudent } from '../controllers/coordinator/students/deleteBanStudent.mjs';
@@ -14,7 +13,6 @@ export const spollnetRouter = express.Router();
 spollnetRouter.post("/register", register);
 spollnetRouter.post("/login", credentials);
 spollnetRouter.post('/check-carnet', checkCarnet);
-spollnetRouter.post('/add-student', addStudent);
 spollnetRouter.delete('/students/:id/permanent', deleteStudentPermanently);
 spollnetRouter.patch('/students/:id/restore', restoreStudent);
 spollnetRouter.get('/students', getAllStudents);
--- frontend/src/components/coordinator/studentsInformation/addStudent.jsx
+++ frontend/src/components/coordinator/studentsInformation/addStudent.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import RegisterButton from '../../../components/student-no-logued/forms/Sign up/registerButton';
 import { Label } from '../../../components/student-no-logued/forms/label';
-import { Input } from '../../../components/student-no-logued/forms/input';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import { useValidations } from '../../../hooks/forms/addStudent';
-
+import { useValidations } from '../../../hooks/forms/forms'
 export default function AddStudent() {
   const { errors, setErrors, handleInputChange, validations, handleBackendErrors } = useValidations();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [identifier, setIdentifier] = useState('');
   const [level, setLevel] = useState('');
   const [specialty, setSpecialty] = useState('');
@@ -17,7 +16,7 @@ export default function AddStudent() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const validationErrors = validations(name, email, level, specialty, identifier);
+    const validationErrors = validations(name, email, password, level, specialty, identifier);
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
       return;
@@ -29,14 +28,14 @@ export default function AddStudent() {
         email: email,
         nivel: level,
         identificador: identifier,
-        especialidad: level === 'Bachillerato' ? specialty : null,
+        password: password,
       };
-      
-      if (level === 'Bachillerato') {
-        data.especialidad = specialty;
-    }
 
-      const response = await axios.post('http://localhost:5000/add-student', data);
+      if (level === 'Bachillerato') {
+        studentData.especialidad = specialty;
+    } 
+
+      const response = await axios.post('http://localhost:5000/register', data);
       Swal.fire({
         title: "¡Éxito!",
         text: "El estudiante ha sido registrado correctamente.",
@@ -44,6 +43,7 @@ export default function AddStudent() {
       });
       setName('');
       setEmail('');
+      setPassword('');
       setIdentifier('');
       setLevel('');
       setSpecialty('');
@@ -62,99 +62,113 @@ export default function AddStudent() {
   };
 
   return (
-    <div>
-      <h2 className="text-2xl font-extrabold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-[#E31FAE] to-[#E4A0D1] shadow-lg py-5">Añadir estudiante</h2>
-      <div className="flex items-center justify-center p-3">
-        <div className="mx-auto w-full max-w-[550px]">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <Label htmlFor="name">Nombre completo</Label>
-              <Input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={handleInputChange(setName)}
-                className={`w-full px-4 py-2 mt-2 rounded-md ${errors.name ? 'border-red-500' : 'border-white'}`}
-              />
-              {errors.name && <p className="text-red-500">{errors.name}</p>}
-            </div>
-
-            <div className="mb-4">
-              <Label htmlFor="email">Correo electrónico</Label>
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={handleInputChange(setEmail)}
-                className={`w-full px-4 py-2 mt-2 rounded-md ${errors.email ? 'border-red-500' : 'border-white'}`}
-              />
-              {errors.email && <p className="text-red-500">{errors.email}</p>}
-            </div>
-
-            <div className="mb-4">
-              <Label htmlFor="identifier">Carnet</Label>
-              <Input
-                type="text"
-                id="identifier"
-                name="identifier"
-                value={identifier}
-                onChange={handleInputChange(setIdentifier)}
-                className={`w-full px-4 py-2 mt-2 rounded-md ${errors.identifier ? 'border-red-500' : 'border-white'}`}
-              />
-              {errors.identifier && <p className="text-red-500">{errors.identifier}</p>}
-            </div>
-
-            <div className="mb-4">
-              <Label htmlFor="level">Nivel educativo</Label>
-              <select
-                id="level"
-                name="level"
-                value={level}
-                onChange={(e) => {
-                  setLevel(e.target.value);
-                  if (e.target.value !== 'Bachillerato') {
-                    setSpecialty('');
-                  }
-                }}
-                className={`w-full px-4 py-2 mt-2 rounded-md ${errors.level ? 'border-red-500' : level ? 'border-green-500' : 'border-white'}`}
-              >
-                <option value="">Seleccione un nivel educativo</option>
-                <option value="Primaria">Primaria</option>
-                <option value="Tercer ciclo">Tercer ciclo</option>
-                <option value="Bachillerato">Bachillerato</option>
-              </select>
-              {errors.level && <p className="text-red-500">{errors.level}</p>}
-            </div>
-
-            <div className="mb-4">
-              <Label htmlFor="specialty">Especialidad</Label>
-              <select
-                id="specialty"
-                name="specialty"
-                value={specialty}
-                onChange={handleInputChange(setSpecialty)}
-                disabled={level !== 'Bachillerato'}
-                className={`w-full px-4 py-2 mt-2 rounded-md ${errors.specialty ? 'border-red-500' : specialty ? 'border-green-500' : 'border-white'}`}
-              >
-                <option value="">Seleccione una especialidad</option>
-                <option value="Mantenimiento automotriz">Mantenimiento automotriz</option>
-                <option value="Desarrollo de software">Desarrollo de software</option>
-                <option value="Atencion primaria en salud">Atencion primaria en salud</option>
-                <option value="Electromecanica">Electromecanica</option>
-                <option value="Diseño Grafico">Diseño Grafico</option>
-                <option value="Electronica">Electronica</option>
-              </select>
-              {errors.specialty && <p className="text-red-500">{errors.specialty}</p>}
-            </div>
-
-            <div className="mt-4">
-              <RegisterButton text="Añadir estudiante" />
-            </div>
-          </form>
+    <>
+        <div className=" min-h-screen">
+          <h2 className="text-2xl font-extrabold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-[#E31FAE] to-[#E4A0D1] shadow-lg py-5">Añadir estudiante</h2>            
+                <form onSubmit={handleSubmit} className="p-10 rounded-lg">
+                    <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                        <div className="text-center font-bold">
+                            <Label htmlFor="name">Nombre completo</Label>
+                            <div className="relative">
+                                <input
+                                    id='name'
+                                    name='name'
+                                    type='text'
+                                    value={name}
+                                    onChange={handleInputChange(setName)}
+                                    className={`w-full px-4 py-2 mt-2 rounded-md bg-black text-[#380B99] font-bold border ${errors.name ? 'border-red-500' : name ? 'border-green-500' : 'border-white'}`}
+                                />
+                                {errors.name && <p className="text-red-500">{errors.name}</p>}
+                            </div>
+                        </div>
+                        <div className="text-center font-bold">
+                            <Label htmlFor="email">Correo electrónico</Label>
+                            <div className="relative">
+                                <input
+                                    id='email'
+                                    name='email'
+                                    type='email'
+                                    value={email}
+                                    onChange={handleInputChange(setEmail)}
+                                    className={`w-full px-4 py-2 mt-2 rounded-md bg-black text-[#380B99] font-bold border ${errors.email ? 'border-red-500' : email ? 'border-green-500' : 'border-white'}`}
+                                />
+                                {errors.email && <p className="text-red-500">{errors.email}</p>}
+                            </div>
+                        </div>
+                        <div className="text-center font-bold">
+                            <Label htmlFor="level">Nivel educativo</Label>
+                            <div className="relative">
+                                <select
+                                    id='level'
+                                    name='level'
+                                    value={level}
+                                    onChange={handleInputChange(setLevel)}
+                                    className={`w-full px-4 py-2 mt-2 rounded-md bg-black text-[#380B99] font-bold border ${errors.level ? 'border-red-500' : level ? 'border-green-500' : 'border-white'}`}
+                                >
+                                    <option value="">Seleccione un nivel educativo</option>
+                                    <option value="Primaria">Primaria</option>
+                                    <option value="Tercer ciclo">Tercer ciclo</option>
+                                    <option value="Bachillerato">Bachillerato</option>
+                                </select>
+                                {errors.level && <p className="text-red-500">{errors.level}</p>}
+                            </div>
+                        </div>
+                        <div className="text-center font-bold">
+                            <Label htmlFor="specialty">Especialidad</Label>
+                            <div className="relative">
+                                <select
+                                    id='specialty'
+                                    name='specialty'
+                                    value={specialty}
+                                    onChange={handleInputChange(setSpecialty)}
+                                    disabled={level !== 'Bachillerato'}
+                                    className={`w-full px-4 py-2 mt-2 rounded-md bg-black text-[#380B99] font-bold border ${errors.specialty ? 'border-red-500' : specialty ? 'border-green-500' : 'border-white'}`}
+                                >
+                                    <option value="">Seleccione una especialidad</option>
+                                    <option value="Mantenimiento automotriz">Mantenimiento automotriz</option>
+                                    <option value="Desarrollo de software">Desarrollo de software</option>
+                                    <option value="Atencion primaria en salud">Atencion primaria en salud</option>
+                                    <option value="Electromecanica">Electromecanica</option>
+                                    <option value="Diseño Grafico">Diseño Grafico</option>
+                                    <option value="Electronica">Electronica</option>
+                                </select>
+                                {errors.specialty && <p className="text-red-500">{errors.specialty}</p>}
+                            </div>
+                        </div>
+                        <div className="text-center font-bold">
+                            <Label htmlFor="identifier">Carnet</Label>
+                            <div className="relative">
+                                <input
+                                    id='identifier'
+                                    name='identifier'
+                                    type='text'
+                                    value={identifier}
+                                    onChange={handleInputChange(setIdentifier)}
+                                    className={`w-full px-4 py-2 mt-2 rounded-md bg-black text-[#380B99] font-bold border ${errors.identifier ? 'border-red-500' : identifier ? 'border-green-500' : 'border-white'}`}
+                                />
+                                {errors.identifier && <p className="text-red-500">{errors.identifier}</p>}
+                            </div>
+                        </div>
+                        <div className="text-center font-bold">
+                            <Label htmlFor="password">Contraseña</Label>
+                            <div className="relative">
+                                <input
+                                    id='password'
+                                    name='password'
+                                    type='password'
+                                    value={password}
+                                    onChange={handleInputChange(setPassword)}
+                                    className={`w-full px-4 py-2 mt-2 rounded-md bg-black text-[#380B99] font-bold border ${errors.password ? 'border-red-500' : password ? 'border-green-500' : 'border-white'}`}
+                                />
+                                {errors.password && <p className="text-red-500">{errors.password}</p>}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-8 text-center">
+                        <RegisterButton text="Registrarse"/>
+                    </div>
+                </form>
         </div>
-      </div>
-    </div>
+    </>
   );
 }
