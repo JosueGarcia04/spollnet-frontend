8eb80facffb674fd3923e42e122f9d4d3c316515
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-10 01:18:42 -0600

    routes for controller

--- backend/routes/router.mjs
+++ backend/routes/router.mjs
@@ -8,6 +8,7 @@ import { getAllStudents } from '../controllers/coordinator/students/getAllStuden
 import { deleteStudent, banStudent } from '../controllers/coordinator/students/deleteBanStudent.mjs';
 import { getDataStudentInformation } from '../controllers/coordinator/students/dataStudentInformation';
 import { updateStudent } from '../controllers/coordinator/students/editStudent.mjs';
+import { getProfile, updateProfile} from '../controllers/students/dataProfile.mjs';
 
 export const spollnetRouter = express.Router();
 
@@ -24,4 +25,6 @@ spollnetRouter.delete('/students/:id', deleteStudent);
 spollnetRouter.patch('/students/:id/ban', banStudent);
 spollnetRouter.get('/dataStudentInformation', getDataStudentInformation);
 spollnetRouter.patch('/students/:id', updateStudent);
+spollnetRouter.Router.get('/profile/:userId', getProfile);
+spollnetRouter.put('/profile/:userId', updateProfile);
 
