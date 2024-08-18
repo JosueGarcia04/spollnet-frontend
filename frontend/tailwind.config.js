02a5e0600979e21c4668b252d7d7f72b2a0104d2
Leonelcj255 <105886610+Leonelcj255@users.noreply.github.com>
2024-08-18 02:58:39 -0600

    Revert "Index"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,32 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-<<<<<<< HEAD:tailwind.config.js
-<<<<<<< Updated upstream
-    extend: {},
-=======
-=======
->>>>>>> main:frontend/tailwind.config.js
     extend: {
       colors: {
         customBlack: '#090909', 
       },
       fontFamily: {
-<<<<<<< HEAD:tailwind.config.js
-        sans: ['YourSansFont', 'sans-serif'],
-        serif: ['YourSerifFont', 'serif'],
-        mono: ['YourMonoFont', 'monospace'],
-        extrabold: ['YourExtraboldFont', 'sans-serif'],
-      },
-    },
->>>>>>> Stashed changes
-=======
         ubuntu: ['Ubuntu', 'sans-serif'],
       },
     },
->>>>>>> main:frontend/tailwind.config.js
   },
   plugins: [],
 }
-
 
