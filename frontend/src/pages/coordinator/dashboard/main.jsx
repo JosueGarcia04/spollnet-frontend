42bc924cca9d66d7c488287640d2516228cf163f
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-18 16:50:36 -0600

    nav in coordinator

--- frontend/src/pages/coordinator/dashboard/main.jsx
+++ frontend/src/pages/coordinator/dashboard/main.jsx
@@ -23,7 +23,7 @@ export const Coordinator =() =>{
     return(
         <>
         <NavbarMobile/>
-        <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-7">
+        <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-16">
             <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
             <div id="menu" className="bg-white/10 col-span-12 md:col-span-3 lg:col-span-2.5 rounded-lg p-2 hidden md:block">
                     <ProfileCoordinatorDashboard onOpenModal={handleOpenModal} />
