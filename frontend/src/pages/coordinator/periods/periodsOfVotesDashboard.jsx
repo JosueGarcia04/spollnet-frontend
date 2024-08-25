89ada02ea4a3a14f2dd59dfd1713be59a9b711a9
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-25 00:35:17 -0600

    period sidebar

--- frontend/src/pages/coordinator/periods/periodsOfVotesDashboard.jsx
+++ frontend/src/pages/coordinator/periods/periodsOfVotesDashboard.jsx
@@ -1,41 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 // Data
 import DataPeriodsInformation from '../../../components/coordinator/periodDashboardInformation/dataPeriodsInformation';
 import ExistingPeriods from '../../../components/coordinator/periodDashboardInformation/existingPeriods';
 import AddPeriod from '../../../components/coordinator/periodDashboardInformation/addPeriod';
 // General
 import ProfileCoordinatorDashboard from '../../../components/coordinator/mainDashboardInformation/profileCoordinatorDashboard';
-import ProfileModal from '../../../components/coordinator/mainDashboardInformation/viewProfileCoordinator/profileModalCoordinator';
 import MenuCoordinatorDashboard from '../../../components/coordinator/mainDashboardInformation/menuCoordinatorDasboard';
 import NavbarMobile from '../../../components/coordinator/mainDashboardInformation/menuMobile/menuMobile';
+import Footer from '../../../components/student-no-logued/general/footer'
 
 export const PeriodsOfVotesDashboard = () => {
-    const [isModalOpen, setModalOpen] = useState(false);
-
-    const handleOpenModal = () => {
-        setModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
 
     return (
-        <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-16">
+        <>
+        <div className="antialiased bg-black w-full min-h-screen text-slate-300 relative py-16 px-5">
             <NavbarMobile />
             <div className="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
-                <div id="menu" className="bg-white/10 col-span-12 md:col-span-4 lg:col-span-3 rounded-lg p-2 hidden md:block">
-                    <ProfileCoordinatorDashboard onOpenModal={handleOpenModal} />
+                    <div
+                        className={`sidebar hidden md:flex md:flex-col md:fixed md:top-14 md:left-0 md:h-full md:w-20 md:bg-[#141414] md:border-r-2 md:border-[#ffffff] md:transition-all md:duration-300 md:ease-in-out`}
+                        style={{ zIndex: 9 }}
+                    >
                     <MenuCoordinatorDashboard />
                 </div>
-                <div id="content" className="bg-white/10 col-span-12 md:col-span-8 lg:col-span-9 rounded-lg p-4">
+                <div id="content" className="bg-white/10 col-span-full md:col-span-full lg:col-span-full rounded-lg p-4 overflow-x-hidden md:ml-20">
                     <DataPeriodsInformation />
                     <ExistingPeriods />
                     <AddPeriod />
                 </div>
             </div>
-            <ProfileModal isOpen={isModalOpen} onClose={handleCloseModal} />
         </div>
+        <Footer/>
+        </>
     );
 }
 
