f569d22982b98baa42af32c71a841d9f10626684
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-25 12:34:33 -0600

    modified btn start

--- frontend/src/components/student-no-logued/general/Btnstart.jsx
+++ frontend/src/components/student-no-logued/general/Btnstart.jsx
@@ -27,9 +27,10 @@ const BackToTopButton = () => {
   };
 
   return (
-    <button
+    
+    <button 
       onClick={handleClick}
-      className={`fixed right-4 p-3 text-[#e7148c] bg-white rounded-full shadow-lg transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'} hidden lg:flex lg:bottom-20 lg:right-4`}
+      className={`fixed right-4 p-3 text-[#e7148c] bg-white rounded-full shadow-lg transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'} hidden lg:flex lg:bottom-24 lg:right-4`}
       style={{ zIndex: 10 }}
     >
       <svg className="w-6 h-6 md:w-8 md:h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
