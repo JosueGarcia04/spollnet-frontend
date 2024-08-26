21c4e225b4d24089019bd62f6d487fd846602bd6
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-25 22:29:55 -0600

    Index

--- frontend/src/pages/student/IndexStudent.jsx
+++ frontend/src/pages/student/IndexStudent.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react"; 
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import NavbarSesionStudent from "../../components/student/navbarSesionStudent";
 import Footer from "../../components/student-no-logued/general/footer";
 import Countdown from "../../components/student-no-logued/index/countdown/timer";
@@ -7,45 +8,70 @@ import Parte1 from '../../components/student-no-logued/index/inicio_index';
 import Parte2 from '../../components/student-no-logued/index/Whatspoll';
 import Parte3 from '../../components/student-no-logued/index/valor_index';
 import Parte4 from '../../components/student-no-logued/index/VotingSteps';
+import Loading from '../../components/loading/loading';
+import ChatbotComponent from '../../components/chat/chatbox';
 import '../../pages/by us/general/extra.css';
-import 'aos/dist/aos.css'; 
-import AOS from 'aos'; 
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 import 'tailwindcss/tailwind.css';
 import 'flowbite';
 
 const IndexStudent = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+      setIsSidebarOpen(prev => !prev);
+    };
+    const [loading, setLoading] = useState(true);
+    const location = useLocation();
+
     useEffect(() => {
         AOS.init();
-        const resetCarousel = () => {
-            const carousel = document.querySelector('[data-carousel="slide"]');
-            if (carousel) {
-                const items = carousel.querySelectorAll('[data-carousel-item]');
-                items.forEach(item => item.classList.add('hidden'));
-                items[0]?.classList.remove('hidden');
-            }
-        };
-        resetCarousel(); 
-        return () => { 
-            // Cleanup if necessary
-        };
-    }, []); 
-    
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [location]);
+
+    useEffect(() => {
+        if (!loading) {
+            const resetCarousel = () => {
+                const carousel = document.querySelector('[data-carousel="slide"]');
+                if (carousel) {
+                    const items = carousel.querySelectorAll('[data-carousel-item]');
+                    items.forEach(item => item.classList.add('hidden'));
+                    items[0]?.classList.remove('hidden');
+                }
+            };
+            resetCarousel();
+        }
+    }, [loading]);
+
     return (
         <>
-            <NavbarSesionStudent/>
-            <div className="bg-black md:flex">
-                <SidebarSesionStudent />
-                <div className="content flex-1 ml-0 md:ml-16 relative top-0">
-                    <div className="text-white text-center mt-5">
-                        <Countdown />
+            {loading ? (
+                <Loading />
+            ) : (
+                <div>
+                     <NavbarSesionStudent toggleSidebar={toggleSidebar} />
+                    <div className="bg-black md:flex">
+                    <SidebarSesionStudent isSidebarOpen={isSidebarOpen} />
+                        <div className="content flex-1 ml-0 md:ml-16 relative top-0">
+                            <div className="text-white text-center mt-5">
+                                <Countdown />
+                            </div>
+                            <Parte1 />
+                            <Parte2 />
+                            <Parte3 />
+                            <Parte4 />
+
+                        </div>
                     </div>
-                    <Parte1 />
-                    <Parte2 />
-                    <Parte3 />
-                    <Parte4 />
+                    <Footer />
+                    <ChatbotComponent/>
                 </div>
-            </div>
-            <Footer />
+            )}
         </>
     );
 };
