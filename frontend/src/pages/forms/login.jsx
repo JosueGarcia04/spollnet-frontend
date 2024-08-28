a8bfe016bcbbc497e5949979c38f3956583718b9
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-28 15:58:13 -0600

    dvsvsdvsdvsdvsd

--- frontend/src/pages/forms/login.jsx
+++ frontend/src/pages/forms/login.jsx
@@ -85,7 +85,7 @@ const Login = () => {
                 });
                 return;
             } else {
-                response = await axios.post('https://spollnet-crea-j-2024.vercel.app/api/login', {
+                response = await axios.post('https://spollnet-backend.vercel.app/controllers/forms/login.mjs', {
                     email: mail,
                     password: contra
                 });
