5ca9584092732edcc093f1ef1d450134c0ab7945
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-29 08:46:11 -0600

    nose

--- frontend/src/components/student/navDownSesionStudent.jsx
+++ frontend/src/components/student/navDownSesionStudent.jsx
@@ -30,14 +30,14 @@ const NavDownSesionStudent = () =>{
   return(
     <>
     <nav className={`fixed bottom-0 left-0 right-0 border-2 border-[#e7148c] bg-black py-2 flex justify-around items-center md:hidden rounded-t-3xl transition-all duration-500 ${isAtBottom && isScrolled ? 'rounded-t-none' : ''}`} style={{position: '-webkit-sticky', position: 'sticky', zIndex: 40}}>
-      <Link to="#" className="text-white hover:text-gray-400">
+      <Link to="/publis" className="text-white hover:text-gray-400">
         <div className="flex items-center">
           <div className="w-12 h-12 flex justify-center items-center">
             <FontAwesomeIcon icon={faChalkboard} className="w-6 h-6 text-white" />
           </div>
         </div>
       </Link>
-      <Link to="/Sign-in" className="text-white hover:text-gray-400">
+      <Link to="#" className="text-white hover:text-gray-400">
         <div className="flex items-center">
           <div className="w-12 h-12 flex justify-center items-center">
             <FontAwesomeIcon icon={faChartBar} className="w-6 h-6 text-white" />
@@ -51,7 +51,7 @@ const NavDownSesionStudent = () =>{
           </div>
         </div>
       </Link>
-      <Link to="/login" className="text-white hover:text-gray-400">
+      <Link to="/student-profile" className="text-white hover:text-gray-400">
         <div className="flex items-center">
           <div className="w-12 h-12 flex justify-center items-center">
             <FontAwesomeIcon icon={faCog} className="w-6 h-6 text-white" />
