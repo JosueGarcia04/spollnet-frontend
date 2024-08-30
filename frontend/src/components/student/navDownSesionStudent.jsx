56ddcd908aa6fcb23fb9709e34a460145fdffd65
JosueGarcia04 <josueadrian042006@gmail.com>
2024-08-30 07:52:22 -0600

    sio

--- frontend/src/components/student/navDownSesionStudent.jsx
+++ frontend/src/components/student/navDownSesionStudent.jsx
@@ -30,7 +30,7 @@ const NavDownSesionStudent = () =>{
   return(
     <>
     <nav className={`fixed bottom-0 left-0 right-0 border-2 border-[#e7148c] bg-black py-2 flex justify-around items-center md:hidden rounded-t-3xl transition-all duration-500 ${isAtBottom && isScrolled ? 'rounded-t-none' : ''}`} style={{position: '-webkit-sticky', position: 'sticky', zIndex: 40}}>
-      <Link to="/publis" className="text-white hover:text-gray-400">
+      <Link to="/public" className="text-white hover:text-gray-400">
         <div className="flex items-center">
           <div className="w-12 h-12 flex justify-center items-center">
             <FontAwesomeIcon icon={faChalkboard} className="w-6 h-6 text-white" />
@@ -44,7 +44,7 @@ const NavDownSesionStudent = () =>{
           </div>
         </div>
       </Link>
-      <Link to="/" className="text-white hover:text-gray-400">
+      <Link to="/Indexstudent" className="text-white hover:text-gray-400">
         <div className="flex items-center">
           <div className="rounded-full bg-[#e7148c] w-12 h-12 flex justify-center items-center">
             <FontAwesomeIcon icon={faHome} className="w-6 h-6 text-white" />
